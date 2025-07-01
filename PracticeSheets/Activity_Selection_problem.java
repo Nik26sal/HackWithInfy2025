81dd5b67ce7ba6c24d99fb263ef7f450e1ead2e0
Nikhil Bansal <114744576+Nik26sal@users.noreply.github.com>
2025-07-01 10:17:49 +0530

    Activity_Selection_problem

---
+++ PracticeSheets/Activity_Selection_problem.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Activity_Selection_Problem
+{
+    public static int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs,(a,b)->a[1]-b[1]);
+        int end = pairs[0][1];
+        int cnt = 1;
+        for(int i =1;i<pairs.length;i++){
+            if(pairs[i][0] > end){
+                end = pairs[i][1];
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+	public static void main(String[] args) {
+	    Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int pairs[][] = new int[n][2];
+		for(int i =0;i<n;i++){
+		    pairs[i][0] = sc.nextInt();
+		    pairs[i][1] = sc.nextInt();
+		}
+		System.out.println(findLongestChain(pairs));
+	}
+}
